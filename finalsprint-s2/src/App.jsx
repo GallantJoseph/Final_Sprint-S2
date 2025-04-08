b67757f725226d6a56e8f23a38035e0d61411119
GallantJoseph <gallant.joseph@outlook.com>
2025-04-08 12:34:09 -0230

    Components and links
    Added BrowserRouter for navigation
    Navigation Component
    PCBuilder Component
    Added some CSS to the Navigation component

--- finalsprint-s2/src/App.jsx
+++ finalsprint-s2/src/App.jsx
@@ -1,8 +1,26 @@
 import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Navigation from "./components/Navigation";
+import Home from "./components/Home";
+import Products from "./components/Products";
+import PCBuilder from "./components/PCBuilder";
+import Cart from "./components/Cart";
+import Contact from "./components/Contact";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Navigation></Navigation>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/Products" element={<Products />}></Route>
+        <Route path="/PCBuilder" element={<PCBuilder />}></Route>
+        <Route path="/Cart" element={<Cart />}></Route>
+        <Route path="/Contact" element={<Contact />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
