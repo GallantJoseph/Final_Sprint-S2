ff22f8e2d3c8a8dc21397217643130c3eb01c3db
TheLostMrJ <greenslade_12@hotmail.com>
2025-04-10 14:46:43 -0230

    bug fix

--- finalsprint-s2/src/components/products/Category.jsx
+++ finalsprint-s2/src/components/products/Category.jsx
@@ -1,40 +1,40 @@
 import { useState, useEffect } from "react";
 
 function Category({ type }) {
-	const [filtData, setFiltData] = useState(null);
+  const [filtData, setFiltData] = useState([]);
 
-	useEffect(() => {
-		async function getData() {
-			const res = await fetch("http://localhost:5000/products");
-			const data = await res.json();
+  useEffect(() => {
+    async function getData() {
+      const res = await fetch("http://localhost:5000/products");
+      const data = await res.json();
 
-			if (type === "all") {
-				setFiltData(data);
-			} else {
-				setFiltData(
-					data.filter((element) => {
-						return element.category == type;
-					})
-				);
-			}
-		}
+      if (type === "all") {
+        setFiltData(data);
+      } else {
+        setFiltData(
+          data.filter((element) => {
+            return element.category == type;
+          })
+        );
+      }
+    }
 
-		getData();
-	}, [type]);
+    getData();
+  }, [type]);
 
-	console.log(filtData);
-	return filtData.map((obj) => (
-		/*/ 
+  console.log(filtData);
+  return filtData.map((obj) => (
+    /*/ 
 			Anything here can be changed for better formatting
 			i just used a ul to get the data on screen
 		/*/
 
-		<ul key={obj.id}>
-			<li>{obj.name}</li>
-			<li>{obj.description}</li>
-			<li>{obj.price.toFixed(2)}</li>
-			{/*/ <li><img src={obj.image} /></li> <-- uncomment when ready to format images /*/}
-		</ul>
-	));
+    <ul key={obj.id}>
+      <li>{obj.name}</li>
+      <li>{obj.description}</li>
+      <li>{obj.price.toFixed(2)}</li>
+      {/*/ <li><img src={obj.image} /></li> <-- uncomment when ready to format images /*/}
+    </ul>
+  ));
 }
 export default Category;
